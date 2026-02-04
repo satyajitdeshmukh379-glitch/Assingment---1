5f787722dc664830f410307e2536f452049136f3
satyajitdeshmukh379-glitch <satyajitdeshmukh379@gmail.com>
2026-02-04 17:37:59 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+
+print("Additioin :",add)
+print("subtraction :",sub)
+print("multiplication :",mul)
+print("division :",div)
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+str1 = input("Enter your First Name  :")
+str2 = input("Enter your Second Name  :")
+
+
+print("Hello",str1+str2,"! Welcome to Python ")
